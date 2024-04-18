c5cc4db13427409b72c2eabaa15a76e680f808cc
Wheatley Minci <qn06142@gmail.com>
2024-04-18 21:47:46 +0700

    modified:   coconut.py 	modified:   running.py

--- coconut.py
+++ coconut.py
@@ -3,3 +3,35 @@ n = int(input())
 monkeyst1 = list(tuple(int(i) for i in input().split()) for i in range(0, n))
 m = int(input())
 monkeyst2 = list(tuple(int(i) for i in input().split()) for i in range(0, m))
+monkeyst1.sort(reverse = True)
+monkeyst2.sort(reverse = True)
+
+def check(x):
+    coconutsharv = 0
+    coconutsproc = 0
+    for i in monkeyst1:
+        if i[0] + i[1] > x:
+            break
+        else:
+            coconutsharv += (x - i[0]) // i[1]
+    for i in monkeyst2:
+        if i[0] + i[1] > (t - x):
+            break
+        else:
+            coconutsproc += ((t - x) - i[0]) // i[1]
+    return coconutsharv, coconutsproc
+"""
+while True:
+    print(check(int(input())))
+"""
+l = 0
+r = t
+while l < r:
+    mid = (l + r) // 2
+    #print(l, r, mid)
+    coconutsharv, coconutsproc = check(mid)
+    if coconutsharv >= coconutsproc:
+        r = mid
+    else:
+        l = mid + 1
+print(l)
--- running.py
+++ running.py
@@ -5,9 +5,9 @@ def count_triplets(positions):
     for i in range(n):
         for j in range(i+1, n):
             for k in range(j+1, n):
-                if abs(positions[j] - positions[i]) >= abs(positions[k] - positions[j]):
+                if abs(positions[j] - positions[i]) <= abs(positions[k] - positions[j]) and abs(positions[j] - positions[i]) * 2 >= abs(positions[k] - positions[j]):
                     count.append((positions[i], positions[j], positions[k]))
     return count
 
 positions = [int(input()) for i in range(0, int(input()))]
-print(len(count_triplets(positions)) - 1)
+print(len(count_triplets(positions)))
