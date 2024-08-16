19d8173151a79053ab35e219a1ce51db6e51d7da
Wheatley <qn06142@gmail.com>
2024-08-16 08:43:31 +0700

    new file:   .cph/.23gl2b5.cpp_e04b2180f4de422cec122c683d5027f2.prob 	new file:   .cph/.A_p_1.py_3391decfbbf5bb6c5dc93986b47a460d.prob 	modified:   .cph/.C_p_3.cpp_72a87fccf3b2a93da538a2e7028b93c7.prob 	modified:   .cph/.D_p_4.cpp_2c7fbd68b6c0fd8f9e6f1e1ebe825d0a.prob 	new file:   .cph/.Meet_in_the_Middle.cpp_bb00097314600e4b27c28fbdb8d88ffd.prob 	new file:   .cph/.demen04.cpp_905041cc5010673a48ebe24ab0f282de.prob 	new file:   .cph/.dowry.cpp_a7754bd148f2e3bf2408b739115b2772.prob 	new file:   23gl2b5 	new file:   23gl2b5.cpp 	new file:   A_p_1.py 	new file:   C_p_3 	modified:   C_p_3.cpp 	modified:   D_p_4.cpp 	new file:   Meet_in_the_Middle 	new file:   Meet_in_the_Middle.cpp 	new file:   demen04.cpp 	new file:   dowry.cpp

---
+++ A_p_1.py
@@ -0,0 +1,2 @@
+input()
+print(len(set(map(int, input().split()))))
--- D_p_4.cpp
+++ D_p_4.cpp
@@ -1,34 +1,51 @@
-#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast,unroll-loops,fast-math")
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
 using namespace std;
-typedef long long ll;
 
-void solve() {
-    ll n; cin >> n;
-    vector<int> a(n);
-    for(auto &it: a) cin >> it;
-
-    priority_queue<int, vector<int>, greater<int>> pq;
-
-    ll res = 0, sum = 0;
-    for(int i = 0; i < n; i++) {
-        sum += a[i];
-        pq.push(a[i]);
-
-        while(sum < 0) { // nếu tổng tiền hiện tại bị âm
-            sum -= pq.top(); // trừ đi số tiền nhỏ nhất
-            pq.pop(); // và loại bỏ nó khỏi hàng đợi
-        }
-
-        res = max(res, sum); // cập nhật lợi nhuận lớn nhất
+int max_sub_arr(const vector<int>& arr) {
+    int max_sum = INT_MIN, current_sum = 0;
+    for (int x : arr) {
+        current_sum = max(x, current_sum + x);
+        max_sum = max(max_sum, current_sum);
     }
-
-    cout << res << endl;
+    return max_sum;
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-
-    solve();
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    vector<int> m(n);
+    
+    for (int i = 0; i < n; ++i) {
+        cin >> m[i];
+    }
+    
+    int result = INT_MIN;
+    
+    for (int maxVal = -500; maxVal <= 500; ++maxVal) {
+        vector<int> transformed;
+        
+        for (int i = 0; i < n; ++i) {
+            if (m[i] > maxVal) {
+                transformed.push_back(INT_MIN / 2);  // Effectively negative infinity
+            } else {
+                transformed.push_back(m[i]);
+            }
+        }
+        
+        int max_sum = max_sub_arr(transformed);
+        if (max_sum != INT_MIN / 2) {
+            result = max(result, max_sum - maxVal);
+        }
+    }
+    
+    cout << result << endl;
+    
+    return 0;
 }
